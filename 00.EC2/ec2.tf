908491eff363e62560a8a0e60644d62a191a8bca
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-11 01:48:24 +0000

    Added Vars

--- 00.EC2/ec2.tf
+++ 00.EC2/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "this" {
-    ami = "ami-090252cbe067a9e58"
-    vpc_security_group_ids = [aws_security_group.this.id]
-    instance_type = "t3.micro"
-    tags = {
-        Name = "db"
-    }
+  ami                    = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [aws_security_group.this.id]
+  instance_type          = "t3.micro"
+  tags = {
+    Name = "db"
+  }
 }
