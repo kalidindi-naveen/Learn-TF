b6cd50322d30f5450ca260d68203d8e66054e8f9
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-11 14:38:32 +0000

    Added Files

---
+++ 12.Provisioner-local/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "this" {
+  ami                    = data.aws_ami.this.id
+  vpc_security_group_ids = ["sg-0d252c8914c14d476"]
+  instance_type          = var.instance_type
+
+  # provisioners will run when you are creating resources
+  # They will not run once the resources are created
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} > IP.txt"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i IP.txt web.yaml"
+  }
+}
