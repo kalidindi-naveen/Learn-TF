767ced589dd7dad0312311c92641dc03daef72cf
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-11 01:53:21 +0000

    Added Vars

--- 01.Variables.tf/variables.tf
+++ 01.Variables.tf/variables.tf
@@ -1,7 +1,3 @@
-#1. command line
-#2. tfvars
-#3. ENV variable
-#4. variable default value
 variable "image_id" {
   type        = string                  #optional
   default     = "ami-090252cbe067a9e58" #optional
