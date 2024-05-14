a5ec2c1e59109ace14606f03ed7c76fec28a9449
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-14 03:07:10 +0000

    Added Files

--- 15.EC2-Module-Use/main.tf
+++ 15.EC2-Module-Use/main.tf
@@ -1,5 +1,5 @@
 module "ec2-made-easy" {
-  source                 = "github.com/kalidindi-naveen/Learn-TF.git//14.EC2-Module?ref=main"
+  source                 = "github.com/kalidindi-naveen/Learn-TF//14.EC2-Module"
   ami                    = data.aws_ami.this.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-0d252c8914c14d476"]
